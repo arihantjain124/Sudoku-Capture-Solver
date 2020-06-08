62dd42a7c88eab05944040110c331dd3e97b12a2
Arihant Jain <14129747+arihantjain124@users.noreply.github.com>
2020-06-08 23:45:51 +0530

    minor changes

--- sudokuread.py
+++ sudokuread.py
@@ -18,25 +18,29 @@ def preProcessing(img):
     return img
 
 
-pickle_in = open("digitread.p", "rb")
-model = pickle.load(pickle_in)
-img = cv2.imread("Test_Images/2.png")
-flag, img = sudokucap(img)
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-puz = []
-temp = []
-for i in range(0, 530, 66):
-    for j in range(0, 530, 66):
-        cell = img[i:i + 66, j:j + 66]
-        cell = cv2.resize(cell, (32, 32))
-        cell = preProcessing(cell)
-        pre = cell.reshape(1, 32, 32, 1)
-        digit = int(model.predict_classes(pre))
-        # print(digit)
-        temp.append(digit)
-        # cv2.imshow("a", cell)
-        # time.sleep(1)
-        cv2.waitKey(1)
-    puz.append(temp)
+def puzzlereader(puzzleimage):
+    pickle_in = open("model.p", "rb")
+    model = pickle.load(pickle_in)
+    # model = tf.keras.models.load_model("mnistnumber.h5")
+    # img = cv2.imread(puzzleimage)
+    flag, img = sudokucap(puzzleimage)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    puz = []
     temp = []
-print(puz)
+    for i in range(0, 530, 66):
+        for j in range(0, 530, 66):
+            cell = img[i:i + 66, j:j + 66]
+            cell = cv2.resize(cell, (32, 32))
+            cell = preProcessing(cell)
+            # cv2.imshow("a", cell)
+            # time.sleep(2)
+            pre = cell.reshape(1, 32, 32, 1)
+            digit = int(model.predict_classes(pre))
+            # print(digit)
+            temp.append(digit)
+            # cv2.imshow("a", cell)
+            # time.sleep(1)
+            cv2.waitKey(1)
+        puz.append(temp)
+        temp = []
+    return puz
